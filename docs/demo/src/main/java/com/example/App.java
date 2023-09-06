7285a7e1f606d09d227418d82f1d99403bd2bc90
Abhishek Dwivedi <abhiietk77@gmail.com>
2023-09-06 12:46:21 +0530

    updated interfaces with no reference to authz spicedb

--- docs/demo/src/main/java/com/example/App.java
+++ docs/demo/src/main/java/com/example/App.java
@@ -1,29 +1,41 @@
 package com.example;
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.authzed.api.v1.Core;
 import com.authzed.api.v1.Core.ObjectReference;
 import com.authzed.api.v1.Core.Relationship;
 import com.authzed.api.v1.Core.RelationshipUpdate;
 import com.authzed.api.v1.Core.SubjectReference;
+import com.authzed.api.v1.Core.ZedToken;
 import com.authzed.api.v1.PermissionService;
-import com.authzed.api.v1.PermissionService.WriteRelationshipsRequest;
-import com.authzed.api.v1.PermissionService.WriteRelationshipsResponse;
+import com.authzed.api.v1.PermissionService.CheckPermissionResponse;
 import com.authzed.api.v1.PermissionsServiceGrpc;
 import com.authzed.grpcutil.BearerToken;
 
+import io.grpc.Channel;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
-
 public class App 
 {
-        public static String manageObjectRelations(PermissionsServiceGrpc.PermissionsServiceBlockingStub paramPermissionService , 
-        String paramRelationType ,  
-        String paramOperationType , 
-        String paramObjectType , 
+        private static final Logger logger = Logger.getLogger(App.class.getName());
+        private static final String target = "localhost:50051";
+        private static final String token = "somerandomkeyhere";
+    
+        private final PermissionsServiceGrpc.PermissionsServiceBlockingStub permissionsService;
+
+        public App(Channel channel) {
+                permissionsService = PermissionsServiceGrpc.newBlockingStub(channel)
+                        .withCallCredentials(new BearerToken(token));
+        }
+        
+        public String manageObjectRelations(String paramRelationType , String paramOperationType , String paramObjectType , 
         String paramObjectId ,String paramSubjectType,String paramSubjectId){
                  // Write relationship
-                WriteRelationshipsRequest relRequest = PermissionService.WriteRelationshipsRequest
+                PermissionService.WriteRelationshipsRequest relRequest = PermissionService.WriteRelationshipsRequest
                 .newBuilder()
                 .addUpdates(
                         RelationshipUpdate.newBuilder()
@@ -48,18 +60,19 @@ public class App
                 .build())
                 .build();
 
-                WriteRelationshipsResponse relResponse = paramPermissionService.writeRelationships(relRequest);
-                String tokenVal = relResponse.getWrittenAt().getToken();
-        
-                return tokenVal;
+                PermissionService.WriteRelationshipsResponse response;
+                try {
+                    response = permissionsService.writeRelationships(relRequest);
+                } catch (Exception e) {
+                    logger.log(Level.WARNING, "RPC failed: {0}", e.getMessage());
+                    return "";
+                }
+                logger.info("Response: " + response.toString());
+                return response.getWrittenAt().getToken();
         }    
         
-        public static boolean checkObjectPermission(PermissionsServiceGrpc.PermissionsServiceBlockingStub paramPermissionService,
-        String paramPermissionType ,  
-        String paramEntityType , 
-        String paramEntityId , String paramObjectType, String paramSubjectId){
-
-                boolean result = false;
+        public boolean checkObjectPermission(String paramPermissionType , String paramEntityType , String paramEntityId 
+        , String paramObjectType, String paramSubjectId){
 
                 PermissionService.CheckPermissionRequest request = PermissionService.CheckPermissionRequest.newBuilder()
                 .setConsistency(
@@ -83,128 +96,127 @@ public class App
                 .build();
 
                 try {
-                        PermissionService.CheckPermissionResponse response = paramPermissionService.checkPermission(request);
+                        PermissionService.CheckPermissionResponse response = permissionsService.checkPermission(request);
                         System.out.println("result: " + response.getPermissionship().getValueDescriptor().getName());
-                        return true;
-                } catch (Exception e) {
-                       System.out.println("Failed to check permission: " + e.getMessage());
-                }
 
-                return false;
-               
+                        if(CheckPermissionResponse.Permissionship.PERMISSIONSHIP_HAS_PERMISSION.getNumber() == (response.getPermissionship().getNumber())){
+                                return true;
+                        } else {
+                                return false;
+                        }
+                } catch (Exception e) {
+                        logger.log(Level.WARNING, "RPC failed: {0}", e.getMessage());
+                        return false;
+                }
         }    
         
         public static void main( String[] args ) 
         {
-                
                 ManagedChannel channel = ManagedChannelBuilder
-                        .forTarget("localhost:50051")
-                        .usePlaintext()
-                        .build();
+                .forTarget(target)
+                .usePlaintext() // if not using TLS, replace with .usePlaintext()
+                .build();
+                try {
+                        App client = new App(channel);
 
-                BearerToken bearerToken = new BearerToken("somerandomkeyhere");
+                        // Write relationships
+                        String result = client.manageObjectRelations(Relation.ADMINISTRATOR.getRelation(),
+                        RelationOperation.CREATE.getRelationOperation(),
+                        ObjectType.CAA.getObjectType(),
+                        "DGCA",
+                        ObjectType.USER.getObjectType(),
+                        "dgca_user");
 
-                PermissionsServiceGrpc.PermissionsServiceBlockingStub permissionsService = PermissionsServiceGrpc
-                .newBlockingStub(channel)
-                .withCallCredentials(bearerToken);
+                        System.out.println(result);
 
-                // Write relationships
-                String result = manageObjectRelations(permissionsService,
-                Relation.ADMINISTRATOR.getRelation(),
-                RelationOperation.CREATE.getRelationOperation(),
-                ObjectType.CAA.getObjectType(),
-                "DGCA",
-                ObjectType.USER.getObjectType(),
-                "dgca_user");
+                        result = client.manageObjectRelations(Relation.ADMINISTRATOR.getRelation(),
+                        RelationOperation.CREATE.getRelationOperation(),
+                        ObjectType.MANUFACTURER.getObjectType(),
+                        "man_1",
+                        ObjectType.USER.getObjectType(),
+                        "man_1_user");
 
-                System.out.println(result);
+                        System.out.println(result);
 
-                result = manageObjectRelations(permissionsService,
-                Relation.ADMINISTRATOR.getRelation(),
-                RelationOperation.CREATE.getRelationOperation(),
-                ObjectType.MANUFACTURER.getObjectType(),
-                "man_1",
-                 ObjectType.USER.getObjectType(),
-                "man_1_user");
+                        result = client.manageObjectRelations(Relation.ADMINISTRATOR.getRelation(),
+                        RelationOperation.CREATE.getRelationOperation(),
+                        ObjectType.MANUFACTURER.getObjectType(),
+                        "man_1",
+                        ObjectType.USER.getObjectType(),
+                        "man_1_user");
 
-                System.out.println(result);
+                        System.out.println(result);
 
-                result = manageObjectRelations(permissionsService,
-                Relation.ADMINISTRATOR.getRelation(),
-                RelationOperation.CREATE.getRelationOperation(),
-                ObjectType.MANUFACTURER.getObjectType(),
-                "man_1",
-                 ObjectType.USER.getObjectType(),
-                "man_1_user");
+                        result = client.manageObjectRelations(Relation.REGULATOR.getRelation(),
+                        RelationOperation.CREATE.getRelationOperation(),
+                        ObjectType.UAS.getObjectType(),
+                        "uas_1",
+                        ObjectType.CAA.getObjectType(),
+                        "dgca");
 
-                System.out.println(result);
+                        System.out.println(result);
 
-                result = manageObjectRelations(permissionsService,
-                Relation.REGULATOR.getRelation(),
-                RelationOperation.CREATE.getRelationOperation(),
-                ObjectType.UAS.getObjectType(),
-                "uas_1",
-                 ObjectType.CAA.getObjectType(),
-                "dgca");
+                        result = client.manageObjectRelations(Relation.MANUFACTURER.getRelation(),
+                        RelationOperation.CREATE.getRelationOperation(),
+                        ObjectType.UAS.getObjectType(),
+                        "uas_1",
+                        ObjectType.MANUFACTURER.getObjectType(),
+                        "man_1");
 
-                System.out.println(result);
+                        System.out.println(result);
 
-                result = manageObjectRelations(permissionsService,
-                Relation.MANUFACTURER.getRelation(),
-                RelationOperation.CREATE.getRelationOperation(),
-                ObjectType.UAS.getObjectType(),
-                "uas_1",
-                 ObjectType.MANUFACTURER.getObjectType(),
-                "man_1");
+                        Thread.sleep(5000);
 
-                System.out.println(result);
+                        boolean permissionResult  = client.checkObjectPermission(Permission.COMMISION_UAS.getPermission(), 
+                        ObjectType.MANUFACTURER.getObjectType(), 
+                        "man_1", 
+                        ObjectType.USER.getObjectType(), 
+                        "man_1_user");
 
-                boolean permissionResult  = checkObjectPermission(permissionsService, 
-                Permission.COMMISION_UAS.getPermission(), 
-                ObjectType.MANUFACTURER.getObjectType(), 
-                "man_1", 
-                ObjectType.USER.getObjectType(), 
-                "man_1_user");
+                        System.out.println(permissionResult);
+                        
+                        permissionResult  = client.checkObjectPermission(Permission.DECOMMISION_UAS.getPermission(), 
+                        ObjectType.UAS.getObjectType(), 
+                        "uas_1", 
+                        ObjectType.USER.getObjectType(), 
+                        "man_1_user");
 
-                System.out.println(permissionResult);
-                
-                permissionResult  = checkObjectPermission(permissionsService, 
-                Permission.DECOMMISION_UAS.getPermission(), 
-                ObjectType.UAS.getObjectType(), 
-                "uas_1", 
-                ObjectType.USER.getObjectType(), 
-                "man_1_user");
+                        System.out.println(permissionResult);
 
-                System.out.println(permissionResult);
+                        permissionResult  = client.checkObjectPermission(Permission.COMMISION_UAS.getPermission(), 
+                        ObjectType.CAA.getObjectType(), 
+                        "DGCA", 
+                        ObjectType.USER.getObjectType(), 
+                        "dgca_user");
 
-                permissionResult  = checkObjectPermission(permissionsService, 
-                Permission.COMMISION_UAS.getPermission(), 
-                ObjectType.CAA.getObjectType(), 
-                "DGCA", 
-                ObjectType.USER.getObjectType(), 
-                "dgca_user");
+                        System.out.println(permissionResult);
 
-                System.out.println(permissionResult);
+                        permissionResult  = client.checkObjectPermission(Permission.DECOMMISION_UAS.getPermission(), 
+                        ObjectType.UAS.getObjectType(), 
+                        "uas_1", 
+                        ObjectType.USER.getObjectType(), 
+                        "dgca_user");
 
-                permissionResult  = checkObjectPermission(permissionsService, 
-                Permission.DECOMMISION_UAS.getPermission(), 
-                ObjectType.UAS.getObjectType(), 
-                "uas_1", 
-                ObjectType.USER.getObjectType(), 
-                "dgca_user");
+                        System.out.println(permissionResult);
+                        
+                        permissionResult  = client.checkObjectPermission(Permission.DECOMMISION_UAS.getPermission(), 
+                        ObjectType.UAS.getObjectType(), 
+                        "uas_1", 
+                        ObjectType.USER.getObjectType(), 
+                        "man_user_1");
 
-                System.out.println(permissionResult);
-                
-                permissionResult  = checkObjectPermission(permissionsService, 
-                Permission.DECOMMISION_UAS.getPermission(), 
-                ObjectType.UAS.getObjectType(), 
-                "uas_1", 
-                ObjectType.USER.getObjectType(), 
-                "man_user_1");
-
-                System.out.println(permissionResult);
+                        System.out.println(permissionResult);
                
-           
+                
+                } catch(InterruptedException exception) {
+                        // Uh oh!
+                }finally {
+                        try {
+                                channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+                        } catch (InterruptedException exception) {
+                                // Uh oh!
+                        }
+                }   
                
         } 
 }
