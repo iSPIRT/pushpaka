72102527614c8129ec0bf0f01c167e2fa5a70637
Abhishek Dwivedi <abhiietk77@gmail.com>
2023-12-22 19:29:04 +0530

    added permission

--- reference-implementation/src/main/java/in/ispirt/pushpaka/authorisation/utils/SpicedbUtils.java
+++ reference-implementation/src/main/java/in/ispirt/pushpaka/authorisation/utils/SpicedbUtils.java
@@ -6,6 +6,6 @@ import com.authzed.api.v1.Core.RelationshipUpdate.Operation;
 public class SpicedbUtils {
     final Operation OPERATION_CREATE = RelationshipUpdate.Operation.OPERATION_CREATE;
     final Operation OPERATION_TOUCH = RelationshipUpdate.Operation.OPERATION_TOUCH;
-
+    final Operation OPERATION_DELETE = RelationshipUpdate.Operation.OPERATION_DELETE;
     
 }
