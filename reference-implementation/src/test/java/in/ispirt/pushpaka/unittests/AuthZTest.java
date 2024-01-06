895bf7d8d8d5029fc08aa85e3e06cffe24c47edc
Abhishek Dwivedi <abhiietk77@gmail.com>
2024-01-06 22:59:08 +0530

    writing unit tests

--- reference-implementation/src/test/java/in/ispirt/pushpaka/unittests/AuthZTest.java
+++ reference-implementation/src/test/java/in/ispirt/pushpaka/unittests/AuthZTest.java
@@ -1,5 +1,32 @@
 package in.ispirt.pushpaka.unittests;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import in.ispirt.pushpaka.authorisation.utils.SpicedbClient;
+
 public class AuthZTest {
+
+	public static SpicedbClient spicedbClient;
+
+	@BeforeClass
+    public static void setup() {
+        spicedbClient = SpicedbClient.getInstance(SpicedbClient.SPICEDDB_TARGET, SpicedbClient.SPICEDB_TOKEN);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+    	try {
+			spicedbClient.shutdownChannel();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    }
+
+	@Test
+	public void testWriteSchema(){
+		spicedbClient.writeSchema(SpicedbClient.SPICEDDB_PERMISSION_FILE);
+	} 
     
 }
